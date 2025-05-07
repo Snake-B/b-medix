293cbcbfeccb35e662c7c1579eb74ed38455696e
Imad Aqil <imaqil.mj@gmail.com>
2025-05-08 06:42:11 +0700

    chat, medicine, cart

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3748586708"
+    serviceWorkerVersion: "1221861117"
   }
 });
